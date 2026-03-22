ca8254e5be6f87fd221bc289e0f7e57c2f279fc0
Rahul <rahulkvadrapalya512005@gmail.com>
2026-03-22 07:15:08 +0530

    modified the some files to fix errors

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,13 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  experimental: {
+    webVitalsAttribution: [],
+  },
+  onDemandEntries: {
+    maxInactiveAge: 60 * 1000,
+    pagesBufferLength: 5,
+  },
 }
 
 export default nextConfig
